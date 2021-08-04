1050286e064c3cffb81cd64d95a295087cd14acb
Marcus <mvsilva1997@gmail.com>
2021-08-03 21:00:52 -0300

    page adm

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmes.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ayoxqpey:7piSTuDTaP8hNx8XREMJUrHu7Pigfcyi@kesavan.db.elephantsql.com/ayoxqpey3'
 db = SQLAlchemy(app)
 
 class Filme(db.Model):
@@ -19,39 +19,44 @@ def index():
     filmes = Filme.query.all()
     return render_template('catalogo.html', filmes=filmes)
 
-@app.route('/<id>')
-def filme_pelo_id(id):
-    filme = Filme.query.get(id)
-    return render_template('catalogo.html', filme=filme)
+@app.route('/adm')
+def adm():
+    filmes = Filme.query.all()
+    return render_template('adm.html', filmes=filmes)
 
-@app.route('/new', methods=['GET', 'POST'])
-def new():
-    if request.method == 'POST':
-        filme = Filme(
-            request.form['nome'],
-            request.form['cartaz']
-            )
-        db.session.add(filme)
-        db.session.commit()
-        return redirect('/#catalogo')
-    return render_template('new.html')
+# @app.route('/<id>')
+# def filme_pelo_id(id):
+#     filme = Filme.query.get(id)
+#     return render_template('catalogo.html', filme=filme)
 
-@app.route('/edit/<id>', methods=['GET', 'POST'])
-def edit(id):
-    filme = Filme.query.get(id)
-    if request.method == 'POST':
-        filme.nome = request.form['nome']
-        filme.cartaz = request.form['cartaz']
-        db.session.commit()
-        return redirect('/#catalogo')
-    return render_template('edit.html', filme=filme)
+# @app.route('/new', methods=['GET', 'POST'])
+# def new():
+#     if request.method == 'POST':
+#         filme = Filme(
+#             request.form['nome'],
+#             request.form['cartaz']
+#             )
+#         db.session.add(filme)
+#         db.session.commit()
+#         return redirect('/#catalogo')
+#     return render_template('new.html')
 
-@app.route('/delete/<id>')
-def delete(id):
-    filme = Filme.query.get(id)
-    db.session.delete(filme)
-    db.session.commit()
-    return redirect('/#catalogo')
+# @app.route('/edit/<id>', methods=['GET', 'POST'])
+# def edit(id):
+#     filme = Filme.query.get(id)
+#     if request.method == 'POST':
+#         filme.nome = request.form['nome']
+#         filme.cartaz = request.form['cartaz']
+#         db.session.commit()
+#         return redirect('/#catalogo')
+#     return render_template('edit.html', filme=filme)
+
+# @app.route('/delete/<id>')
+# def delete(id):
+#     filme = Filme.query.get(id)
+#     db.session.delete(filme)
+#     db.session.commit()
+#     return redirect('/#catalogo')
 
 if __name__ == '__main__':
     db.create_all()
