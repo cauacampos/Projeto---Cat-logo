d1ec09ce2f32ba14416a287eb93f7ba55dce7188
carlos <carlos.wesley.jesus@gmail.com>
2021-08-05 19:20:12 -0300

    att

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,4 @@
-var audio = new Audio('./music/Netflix.mp3')
+let audio = new Audio('../music/Netflix.mp3')
 let btnEnviar = document.querySelector('#enviar')
 
 btnEnviar.addEventListener('click', () => {
